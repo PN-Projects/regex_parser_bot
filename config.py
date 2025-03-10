bf5a30e60ee27c368e48007d643486ca63932e4f
Parthiv Katapara <83153745+satyanandatripathi@users.noreply.github.com>
2025-03-11 01:04:46 +0530

    setting up config environment

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
@@ -9,3 +8,7 @@ API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 DATABASE_URI = os.getenv('DATABASE_URI')
+
+# Privileged users and god user
+GOD_USER_ID = int(os.getenv('GOD_USER_ID'))  # God user ID
+PRIVILEGED_USER_IDS = list(map(int, os.getenv('PRIVILEGED_USER_IDS', '').split(',')))  # List of privileged user IDs
