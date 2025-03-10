afd771daa73fe0e9201bfbe2fbd5f09feccb3a16
Parthiv Katapara <83153745+satyanandatripathi@users.noreply.github.com>
2025-03-11 01:10:04 +0530

    iniiiittttt

---
+++ commands/__init__.py
@@ -0,0 +1,9 @@
+PACKAGE_NAME = "Regex Parser Bot Commands"
+PACKAGE_VERSION = "1.0.0"
+
+from .start import *
+from .help import *
+from .regex import *
+from .broadcast import *
+from .stats import *
+from .admin import *
